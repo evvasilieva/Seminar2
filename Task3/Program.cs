21396b6c31c3ddf2c36470d94bf13ee0fb4247a7
vectorpy <vectorpy@mail.ru>
2023-09-07 09:50:31 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15 (Task 3): Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите цифру, обозначающую день недели: "); 
+int dayWeek = Convert.ToInt32(Console.ReadLine());
+
+if (dayWeek == 6 || dayWeek == 7)
+{
+    Console.WriteLine("{0} -> Да, этот день - выходной", dayWeek);
+}
+else if (dayWeek >= 1 && dayWeek <= 5)
+{
+    Console.WriteLine("{0} -> Нет, этот день не является выходным", dayWeek);
+}
+else
+{
+    Console.WriteLine("{0} -> дня недели с такой цифрой нет", dayWeek);
+}
