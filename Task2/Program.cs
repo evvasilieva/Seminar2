32fb7c183584005017684c98c972d2f196815f6b
vectorpy <vectorpy@mail.ru>
2023-09-07 09:35:27 +0300

    Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13 (Task1): Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: "); 
+string number = Console.ReadLine();
+
+char[] numberArray = number.ToCharArray();
+if (numberArray.Length >= 3)
+{
+    Console.Write($"{number} -> {numberArray[2]}");
+}
+else
+{
+    Console.Write($"{number} -> Tретьей цифры нет");
+}
